81e1e0ec6bcc6de949ad8436bc4742c347635415
Denis Belavin <denis.belavin@saritasa.com>
2024-11-17 14:41:43 +0700

    Change test os

--- calculator/TestCalculator.py
+++ calculator/TestCalculator.py
@@ -1,20 +1,24 @@
 import pytest
 from BankCalculator import BankCalculator as bc
 
+
 def test_calculate_monthly_payment_regular_case():
     """Тестируем расчет для обычных данных"""
     result = bc.calculate_monthly_payment(100000, 5, 20)
     assert result == pytest.approx(659.96, rel=1e-2)
+
 
 def test_calculate_monthly_payment_zero_interest():
     """Тест для ставки 0%"""
     result = bc.calculate_monthly_payment(120000, 0, 20)
     assert result == 120000 / (20 * 12)
 
+
 def test_calculate_monthly_payment_large_loan():
     """Тест для большого кредита"""
     result = bc.calculate_monthly_payment(1000000, 7, 30)
     assert result == pytest.approx(6652.42, rel=1e-2)
+
 
 def test_calculate_monthly_payment_small_loan():
     """Тест для маленького кредита"""
