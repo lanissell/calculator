2bb4b701a2cf69337b7a1e77b9d970586f5df2a8
Denis Belavin <denis.belavin@saritasa.com>
2024-11-17 13:27:36 +0700

    Add tests

--- calculator/BankCalculator.py
+++ calculator/BankCalculator.py
@@ -5,6 +5,8 @@ class BankCalculator:
 
         r = annual_rate / 100 / 12
         n = years * 12
+
         if r == 0:
             return principal / n
+
         return principal * r * (1 + r) ** n / ((1 + r) ** n - 1)
